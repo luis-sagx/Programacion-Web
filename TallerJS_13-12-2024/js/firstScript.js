36c512b950a83494806e1254858e3968092c567f
Luis Sagnay <sagnayluis99@gmail.com>
2024-12-13 12:46:00 -0500

    js

---
+++ TallerJS_13-12-2024/js/firstScript.js
@@ -0,0 +1,23 @@
+var firtName = "Luis";
+var lastName = "Sagnay"
+age = 20;
+
+console.log("Mi nobre es " + firtName + " " + lastName);
+console.log("Mi edad es " +age)
+
+let x = 10;
+let y = 20;
+let z = x + y;
+console.log(z);
+
+// var declara una variable global
+// let declara variable local o de bloque
+// contsdclara variables constantes que no cambian 
+// if 
+// switch
+// for
+// while
+// funtion marca un bloque de instrucciones
+// try se ejecuta cuando no se produce ningun error 
+// catch se ejecuta cuando se produce algun error
+// return se ejecuta cuando una condicion se cumple, retorna un valor 
